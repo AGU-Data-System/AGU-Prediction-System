b25f3bbb9a9fd4877364f1c21227e27b48d05bc3
Sideghost <guilherme.cordeiro.pereira@gmail.com>
2024-06-07 22:11:41 +0100

    Spring Initializr.

---
+++ code/jvm/src/main/kotlin/aguPredictionSystem/server/AguPredictionSystemApplication.kt
@@ -0,0 +1,11 @@
+package aguPredictionSystem.server
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class AguPredictionSystemApplication
+
+fun main(args: Array<String>) {
+	runApplication<AguPredictionSystemApplication>(*args)
+}
---
+++ code/jvm/src/test/kotlin/aguPredictionSystem/server/AguPredictionSystemApplicationTests.kt
@@ -0,0 +1,13 @@
+package aguPredictionSystem.server
+
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest
+class AguPredictionSystemApplicationTests {
+
+	@Test
+	fun contextLoads() {
+	}
+
+}
